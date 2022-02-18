d2efa6c706cdffa565999b92e8076f1f41c54c44
Matthieu CODRON <matthieu.codron@calmedica.fr>
2022-02-18 17:17:48 +0100

    no-output

--- subdir/test.sh
+++ subdir/test.sh
@@ -8,42 +8,16 @@ git restore src
 cat << 'EOF' > ./src/test.php
 <?php
 
-$a =        "foo";
+$a =        "foo"
 $b =        "bar";
 EOF
 
 git add ./src/test.php
 
-cat << 'EOF' > ./src/test.php
-<?php
-
-$a =        "foo";
-$c =        "baz";
-EOF
+echo "----------------------------------------"
+echo "manually running the pre-commit hook"
+(cd .. && .git/hooks/pre-commit)
 
 echo "----------------------------------------"
-echo "git status before commit"
-echo
-
-git status
-
-echo "----------------------------------------"
-echo "file content before commit"
-echo
-
-cat ./src/test.php
-
+echo "commit"
 git commit -m 'test'
-
-status="$?"
-
-echo "----------------------------------------"
-echo "git status after failed commit ($status)"
-echo
-git status
-
-echo "----------------------------------------"
-echo "file content after commit"
-echo
-
-cat ./src/test.php
